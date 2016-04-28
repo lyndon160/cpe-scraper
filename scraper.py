4c5e085c4ec0ea47c9ac426c57b5b9fd7d3689d0
Lyndon Fawcett <lyndonfawcett@hotmail.com>
2016-04-28 19:01:47 +0100

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Scrapper:
+
+        def collectDeviceLinks(soup):
+		print "Collecting"
+		global devices
+                group = soup.find("div", {"class": "mw-category"})
+                for div in group:
+                        links = div.findAll('a')
+                        for a in links:
+                                    devices.append("https://wikidevi.com/w/index.php?title=Category:Wireless_embedded_system&pageuntil=AirVast+WR-2000" + a['href'])
+                return
+
+
+
+
+	r  = requests.get("https://wikidevi.com/w/index.php?title=Category:Wireless_embedded_system&pageuntil=AirVast+WR-2000#mw-pages")
+	data = r.text
+	soup = BeautifulSoup(data, 'lxml')
+	global devices
+	devices = []
+	turnPageDiv = soup.find("div", {"id": "mw-pages"})
+	collectDeviceLinks(soup)
+        r  = requests.get("https://wikidevi.com/w/index.php?title=Category:Wireless_embedded_system&pageuntil=AirVast+WR-2000" + turnPageDiv.findAll('a')[0]['href'])
+        data = r.text
+        soup = BeautifulSoup(data, 'lxml')
+
+	collectDeviceLinks(soup)
+
+        turnPageDiv = soup.find("div", {"id": "mw-pages"})
+
+
+
+	for i in range(1, 5):
+		r  = requests.get("https://wikidevi.com/w/index.php?title=Category:Wireless_embedded_system&pageuntil=AirVast+WR-2000" + turnPageDiv.findAll('a')[1]['href'])
+		data = r.text
+        	soup = BeautifulSoup(data, 'lxml')
+        	collectDeviceLinks(soup)
+		turnPageDiv = soup.find("div", {"id": "mw-pages"})
+		print i
+
+
