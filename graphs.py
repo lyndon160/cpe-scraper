15621619b6504df7992eb890e39fdcc5b0253bcf
Lyndon John Fawcett <lyndonfawcett@hotmail.com>
2016-07-01 12:29:38 +0100

    Add line to CPU graph

--- graphs.py
+++ graphs.py
@@ -2,9 +2,44 @@
 import matplotlib.pyplot as plt
 import pickle
 
-fileObject = open('rawResults.txt','r')  
+import numpy as np
+
+from numpy import *
+from matplotlib.pyplot import *
+
+
+
+
+
+#average results per year
+def averageResults(a , b):
+    #for every duplicate a add the b equivalent
+    matches = zip (a, b)
+    #Find unique dates
+    dates = list(set(a))
+    histDict = {}
+    #Collect all results with that date
+    for d in dates:
+        histDict[d] = 0
+        count = 0
+        for m in matches:
+            if(m[0] == d):
+                histDict[d] = histDict[d] + m[1]
+                count+=1
+        #Average that hist
+        if(histDict[d]):
+            print(d, histDict[d], count, histDict[d]/count)
+            histDict[d]=histDict[d]/count
+    return histDict
+
+
+
+
+
+
+fileObject = open('rawResults.txt','r')
 # load the object from the file into var b
-deviceList = pickle.load(fileObject)  
+deviceList = pickle.load(fileObject)
 
 #Get last 4 digits of date (Year)
 dates = []
@@ -17,7 +52,38 @@ for d in deviceList:
 	RAM.append(float(d.get('Memory').strip('MiB')))
 	ROM.append(float(d.get('Flash').strip('MiB')))
 
+print deviceList
+
 print dates, CPU
+
+cpuAverage = averageResults(dates, CPU)
+
+print cpuAverage
+
+
+
+lineWeight=5
+coefficients = polyfit(np.array(cpuAverage.keys()), np.array(cpuAverage.values()), 3)
+
+polynomial = np.poly1d(coefficients)
+xs = np.linspace(2016, 1998, 50, endpoint=True)
+ys = polynomial(xs)
+plt.plot(np.array(cpuAverage.keys()), np.array(cpuAverage.values()),'o', xs, ys, linewidth=lineWeight)
+plt.ylabel('CPU MHz')
+plt.xlabel('Years')
+plt.show()
+
+
+
+
+plt.scatter(cpuAverage.keys(), cpuAverage.values())
+
+plt.ylabel('CPU MHz')
+plt.xlabel('Years')
+plt.show()
+
+
+
 
 plt.scatter(dates, CPU)
 
--- scraper.py
+++ scraper.py
@@ -32,68 +32,65 @@ class Scrapper:
     turnPageDiv = soup.find("div", {"id": "mw-pages"})
 
     #collect device pages
-    for i in range(1, 75):
+    for i in range(1, 19):
         r  = requests.get("https://wikidevi.com/w/index.php?title=Category:Wireless_embedded_system&pageuntil=AirVast+WR-2000" + turnPageDiv.findAll('a')[1]['href'])
         data = r.text
         soup = BeautifulSoup(data, 'lxml')
         collectDeviceLinks(soup)
         turnPageDiv = soup.find("div", {"id": "mw-pages"})
-    	print i
-    
+        print i
+
     deviceList = []
 
     #Goto each device page and get details
     for index, deviceLink in enumerate(devices):
-	print (index)
-        #print deviceLink
-        r = requests.get(deviceLink)
-        data = r.text
-        soup = BeautifulSoup(data, 'lxml')
-        text = soup.find("div", {"id": "mw-content-text"})
-	x = 0
-	date = ""
-	cpu = ""
-	ram = ""
-	rom = ""
-	#Search through side table
-	for i in text.contents[0].contents:
-		#Get specs
-		if("CPU" in unicode(i)):
-			for c in i.contents:
-				if("MHz" in unicode(c)):
-					cpu = unicode(c).strip('<b>').strip('</b>')
-		if("FLA" in unicode(i)):
-			for c in i.contents:
-				if("MiB" in unicode(c) and not rom):
-					rom = c.contents
-				elif("MiB" in unicode(c) and not ram):
-					ram = c.contents
+        try:
+            print (index)
+            r = requests.get(deviceLink)
+            data = r.text
+            soup = BeautifulSoup(data, 'lxml')
+            text = soup.find("div", {"id": "mw-content-text"})
+            x = 0
+            date = ""
+            cpu = ""
+            ram = ""
+            rom = ""
+            #Search through side table
+            for i in text.contents[0].contents:
+                #Get specs
+                if("CPU" in unicode(i)):
+                    for c in i.contents:
+                        if("MHz" in unicode(c)):
+                            cpu = unicode(c).strip('<b>').strip('</b>')
+                if("FLA" in unicode(i)):
+                    for c in i.contents:
+                        if("MiB" in unicode(c) and not rom):
+                            rom = c.contents
+                        elif("MiB" in unicode(c) and not ram):
+                            ram = c.contents
 
-		#Get date		
-		if("FCC" in unicode(i)):
-			for d in i.contents:
-				for m in ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]:
-					if(m in unicode(d)):
-						date = d
-		if (date and cpu and rom and ram):
-			#print (unicode(date) + " CPU " + unicode(cpu) + " Flash " + unicode(rom[0].contents[0].contents[0]) + " Memory " + unicode(ram[0].contents[0].contents[0]))#.strip('<span class="smwtext">()').strip('</span>'))
-			#deviceStats = {unicode(date), unicode(cpu).strip("MHz"), unicode(rom[0].contents[0].contents[0]).strip("MiB"), unicode(ram[0].contents[0].contents[0]).strip("MiB")}
-			deviceStats = {}
-			deviceStats['URL'] = deviceLink
-			deviceStats['Date'] = unicode(date).strip()
-			deviceStats['CPU'] = unicode(cpu).strip("MHz").strip()
-			deviceStats['Flash'] = unicode(rom[0].contents[0].contents[0]).strip("MiB").strip()
-			deviceStats['Memory'] = unicode(ram[0].contents[0].contents[0]).strip("MiB").strip()
-			deviceList.append(deviceStats)
-			#print deviceStats
-			break
-		x+=1
-		
-
-
-    print deviceList
-    fileObject =  open('rawResults.txt', 'wb')
-    pickle.dump(deviceList, fileObject)
-    fileObject.close()
-
-
+                #Get date
+                if("FCC" in unicode(i)):
+                    for d in i.contents:
+                        for m in ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]:
+                            if(m in unicode(d)):
+                                date = d
+                if (date and cpu and rom and ram):
+                    #print (unicode(date) + " CPU " + unicode(cpu) + " Flash " + unicode(rom[0].contents[0].contents[0]) + " Memory " + unicode(ram[0].contents[0].contents[0]))#.strip('<span class="smwtext">()').strip('</span>'))
+                    #deviceStats = {unicode(date), unicode(cpu).strip("MHz"), unicode(rom[0].contents[0].contents[0]).strip("MiB"), unicode(ram[0].contents[0].contents[0]).strip("MiB")}
+                    deviceStats = {}
+                    deviceStats['URL'] = deviceLink
+                    deviceStats['Date'] = unicode(date).strip()
+                    deviceStats['CPU'] = unicode(cpu).strip("MHz").strip()
+                    deviceStats['Flash'] = unicode(rom[0].contents[0].contents[0]).strip("MiB").strip()
+                    deviceStats['Memory'] = unicode(ram[0].contents[0].contents[0]).strip("MiB").strip()
+                    deviceList.append(deviceStats)
+                    #print deviceStats
+                    break
+                x+=1
+            print deviceList
+            fileObject =  open('rawResults.txt', 'wb')
+            pickle.dump(deviceList, fileObject)
+            fileObject.close()
+        except:
+            print "Unexpected error"
